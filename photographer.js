d3a9510489f27b451d1aad4ab47401c13c610207
labajo <labajo@tid.es>
2016-02-26 11:41:18 +0100

    ADD photographer

---
+++ photographer.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var RaspiCam = require("raspicam");
+
+module.exports = function takePhoto(err, callback) {
+
+  var isRPi = process.arch === 'arm';
+
+  if (isRPi) {
+    var currTime = new Date().getTime();
+    var pictureFilename = "/home/pi/pictures/" + currTime + ".jpg";
+
+    var opts = {
+      mode: "photo",
+      quality: 10,
+      width: 1024,
+      height: 768,
+      output: pictureFilename
+    };
+
+    var camera = new RaspiCam(opts);
+    camera.start(opts);
+    camera.on('read', function(err, timestamp, filename){
+      // Read event is called twice taking one photo. Avoid to stop the cam process at the first event.
+      if (filename.indexOf('~') < 0) {
+        camera.removeListener('read', function() {
+          console.log('Removed listener.');
+        });
+        camera.stop();
+        console.log('Camera stopped.');
+        callback(err, filename);
+      }
+    });
+  } else {
+    // Non raspberry environments.
+    callback(null, './test.jpg');
+  }
+};
