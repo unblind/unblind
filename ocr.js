7332aec7aad8a4b3f853bb10864c51f6fa4add9b
Guido García <guido.garciabernardo@telefonica.com>
2016-02-26 18:56:36 +0100

    cleanup

--- ocr.js
+++ ocr.js
@@ -3,12 +3,18 @@
 const talker = require('./talker');
 const firebaser = require('./firebaser');
 
-const ENABLE_GOOGLE = true;
-const ENABLE_MS = false;
+const vision = require('node-cloud-vision-api');
+
+const GOOGLE_VISION_API_TOKEN = process.env.GOOGLE_VISION_API_TOKEN;
+
+if (!GOOGLE_VISION_API_TOKEN) {
+  console.error('You need to set a GOOGLE_VISION_API_TOKEN env var');
+  process.exit(1);
+}
+
+vision.init({auth: GOOGLE_VISION_API_TOKEN});
 
 var photographer = require('./photographer.js');
-
-talker.speak('Entendido. Voy a intentar entender lo que tienes delante. Ahora te lo leo.');
 
 photographer.takePhoto(function(err, photoFileName) {
   if (err) {
@@ -16,99 +22,45 @@ photographer.takePhoto(function(err, photoFileName) {
     return;
   }
 
+  talker.speak('Entendido. Voy a intentar entender lo que tienes delante. Ahora te lo leo.');
+
   console.log('File: ' + photoFileName);
 
-  //
-  // Google Vision API
-  //
-  if (ENABLE_GOOGLE) {
-    const vision = require('node-cloud-vision-api');
+  const req = new vision.Request({
+    image: new vision.Image(photoFileName),
+    features: [
+      new vision.Feature('TEXT_DETECTION', 10)
+    ]
+  });
 
-    const GOOGLE_VISION_API_TOKEN = process.env.GOOGLE_VISION_API_TOKEN;
+  firebaser.uploadImage(photoFileName, {}); // best effort: fire and forget
 
-    if (!GOOGLE_VISION_API_TOKEN) {
-      console.error('You need to set a GOOGLE_VISION_API_TOKEN env var');
-      process.exit(1);
-    }
+  vision.annotate(req).then((res) => {
+    console.log(JSON.stringify(res.responses));
 
-    vision.init({auth: GOOGLE_VISION_API_TOKEN});
+    // use the first response
+    if (Array.isArray(res.responses) && res.responses.length > 0) {
+      let response = res.responses[0];
 
-    const req = new vision.Request({
-      image: new vision.Image(photoFileName),
-      features: [
-        new vision.Feature('TEXT_DETECTION', 10)
-      ]
-    });
-
-    firebaser.uploadImage(photoFileName, {}); // best effort: fire and forget
-
-    vision.annotate(req).then((res) => {
-      console.log(JSON.stringify(res.responses));
-
-      // use the first response
-      if (Array.isArray(res.responses) && res.responses.length > 0) {
-        let response = res.responses[0];
-
-        if (Array.isArray(response.textAnnotations) && response.textAnnotations.length > 0) {
-          let spokenText = response.textAnnotations[0].description;
-          spokenText = spokenText.replace(/\n/g, ". ");
-          spokenText = fixString(spokenText);
-          console.log('speak %s', spokenText);
-          talker.speak('Atiende, este es el texto que he leido: ' + spokenText);
-        } else {
-          console.log('No text annotations');
-          talker.speak('No consigo entender ese texto');
-        }
+      if (Array.isArray(response.textAnnotations) && response.textAnnotations.length > 0) {
+        let spokenText = response.textAnnotations[0].description;
+        spokenText = spokenText.replace(/\n/g, ". ");
+        spokenText = fixString(spokenText);
+        console.log('speak %s', spokenText);
+        talker.speak('Atiende, este es el texto que he leido: ' + spokenText);
       } else {
-        console.log('No responses');
-        talker.speak('Lo siento, no consigo entender ese texto');
+        console.log('No text annotations');
+        talker.speak('No consigo entender ese texto');
       }
-    }, (e) => {
-      console.log('Error: ', e);
-    }).catch((e) => {
-      console.log('ERROR FATAL', e);
-    });
-  }
-
-  //
-  // MS Oxford
-  //
-  if (ENABLE_MS) {
-    var oxford = require('project-oxford');
-
-    const MS_VISION_COMPUTER_API_TOKEN = process.env.MS_VISION_COMPUTER_API_TOKEN;
-
-    if (!MS_VISION_COMPUTER_API_TOKEN) {
-      console.error('You need to set a MS_VISION_COMPUTER_API_TOKEN env vars');
-      process.exit(1);
+    } else {
+      console.log('No responses');
+      talker.speak('Lo siento, no consigo entender ese texto');
     }
-
-    const clientComputer = new oxford.Client(MS_VISION_COMPUTER_API_TOKEN);
-
-    clientComputer.vision.ocr({
-      path: photoFileName,
-      language: 'es'
-    }).then(function (visionResponse) {
-      console.log(JSON.stringify(visionResponse));
-
-      firebaser.uploadImage(photoFileName, visionResponse); // best effort: fire and forget
-
-      if (visionResponse.regions) {
-        visionResponse.regions.forEach((region) => {
-          console.log(region.lines.words);
-        });
-      }
-
-      if (visionResponse.words) {
-        console.log('HAY WORDS');
-        visionResponse.words.forEach((word) => {
-          console.log(word.text);
-        });
-      }
-    }, (e) => {
-      console.log('Error: ', e);
-    });;
-  }
+  }, (e) => {
+    console.log('Error: ', e);
+  }).catch((e) => {
+    console.log('ERROR FATAL', e);
+  });
 });
 
 // see http://stackoverflow.com/a/20444401/12388
--- visionapi.js
+++ visionapi.js
@@ -3,12 +3,32 @@
 const talker = require('./talker');
 const firebaser = require('./firebaser');
 
-const ENABLE_GOOGLE = false;
-const ENABLE_MS = true;
+const photographer = require('./photographer.js');
 
-var photographer = require('./photographer.js');
+const vision = require('node-cloud-vision-api');
+const oxford = require('project-oxford');
 
-// talker.speak('De acuerdo. Voy a analizar lo que tienes delante. Ahora te cuento.');
+const GOOGLE_VISION_API_TOKEN = process.env.GOOGLE_VISION_API_TOKEN;
+
+if (!GOOGLE_VISION_API_TOKEN) {
+  console.error('You need to set a GOOGLE_VISION_API_TOKEN env var');
+  process.exit(1);
+}
+
+vision.init({auth: GOOGLE_VISION_API_TOKEN});
+
+const MS_VISION_FACE_API_TOKEN = process.env.MS_VISION_FACE_API_TOKEN;
+const MS_VISION_EMOTION_API_TOKEN = process.env.MS_VISION_EMOTION_API_TOKEN;
+const MS_VISION_COMPUTER_API_TOKEN = process.env.MS_VISION_COMPUTER_API_TOKEN;
+
+if (!MS_VISION_FACE_API_TOKEN || !MS_VISION_EMOTION_API_TOKEN || !MS_VISION_COMPUTER_API_TOKEN) {
+  console.error('You need to set a MS_VISION_FACE_API_TOKEN and MS_VISION_EMOTION_API_TOKEN and MS_VISION_COMPUTER_API_TOKEN env vars');
+  process.exit(1);
+}
+
+const clientFaces = new oxford.Client(MS_VISION_FACE_API_TOKEN),
+      clientEmotions = new oxford.Client(MS_VISION_EMOTION_API_TOKEN),
+      clientComputer = new oxford.Client(MS_VISION_COMPUTER_API_TOKEN);
 
 photographer.takePhoto(function(err, photoFileName) {
   if (err) {
@@ -16,143 +36,83 @@ photographer.takePhoto(function(err, photoFileName) {
     return;
   }
 
+  talker.speak('De acuerdo. Voy a analizar lo que tienes delante. Ahora te cuento.');
+
 	console.log('File: ' + photoFileName);
 
   firebaser.uploadImage(photoFileName, {}); // best effort: fire and forget
 
-  //
-  // Google Vision API
-  //
-  if (ENABLE_GOOGLE) {
-    const vision = require('node-cloud-vision-api');
+  /**
+  clientComputer.vision.analyzeImage({
+    path: photoFileName,
+    Categories: true,
+    Faces: true
+  }).then((res) => {
+    console.log(JSON.stringify(res));
+  }, (e) => {
+    console.log('Error: ', e);
+  });
+  */
 
-    const GOOGLE_VISION_API_TOKEN = process.env.GOOGLE_VISION_API_TOKEN;
-
-    if (!GOOGLE_VISION_API_TOKEN) {
-      console.error('You need to set a GOOGLE_VISION_API_TOKEN env var');
-      process.exit(1);
-    }
-
-    vision.init({auth: GOOGLE_VISION_API_TOKEN});
-
-    const req = new vision.Request({
-      image: new vision.Image(photoFileName),
-      features: [
-        new vision.Feature('LABEL_DETECTION', 10),
-        new vision.Feature('LOGO_DETECTION', 5),
-        new vision.Feature('LANDMARK_DETECTION', 5)
-      ]
-    });
-
-    vision.annotate(req).then((res) => {
-      console.log(JSON.stringify(res.responses));
-    }, (e) => {
-      console.log('Error: ', e);
-    });
-  }
-
-  //
-  // MS Oxford
-  //
-  if (ENABLE_MS) {
-    var oxford = require('project-oxford');
-
-    const MS_VISION_FACE_API_TOKEN = process.env.MS_VISION_FACE_API_TOKEN;
-    const MS_VISION_EMOTION_API_TOKEN = process.env.MS_VISION_EMOTION_API_TOKEN;
-    const MS_VISION_COMPUTER_API_TOKEN = process.env.MS_VISION_COMPUTER_API_TOKEN;
-
-    if (!MS_VISION_FACE_API_TOKEN || !MS_VISION_EMOTION_API_TOKEN || !MS_VISION_COMPUTER_API_TOKEN) {
-      console.error('You need to set a MS_VISION_FACE_API_TOKEN and MS_VISION_EMOTION_API_TOKEN and MS_VISION_COMPUTER_API_TOKEN env vars');
-      process.exit(1);
-    }
-
-    const clientFaces = new oxford.Client(MS_VISION_FACE_API_TOKEN),
-          clientEmotions = new oxford.Client(MS_VISION_EMOTION_API_TOKEN),
-          clientComputer = new oxford.Client(MS_VISION_COMPUTER_API_TOKEN);
-
-    clientComputer.vision.analyzeImage({
+  clientFaces.face.detect({
       path: photoFileName,
-      Categories: true,
-      Faces: true
-    }).then((res) => {
-      console.log(JSON.stringify(res));
-    }, (e) => {
-      console.log('Error: ', e);
-    });
+      analyzesAge: true,
+      analyzesGender: true,
+      analyzesSmile: true,
+      analyzesFacialHair: true
+  }).then((res) => {
+    console.log(JSON.stringify(res));
 
-    clientFaces.face.detect({
-        path: photoFileName,
-        analyzesAge: true,
-        analyzesGender: true,
-        analyzesSmile: true,
-        analyzesFacialHair: true
-    }).then((res) => {
-      console.log('Faces found %d', res.length);
+    if (res.length > 0) {
+      // process people
+      let generalDescription = 'Ahora mismo tienes a ' + (res.length === 1 ? 'una persona' : res.length + ' personas') + ' delante.';
 
-      console.log(JSON.stringify(res));
-      if (res.length > 0) {
-        // process people
+      talker.speak(generalDescription, function () {
+        if (res.length < 4) { // avoid parsing too many people
+          let description = describeOxfordPeople(res);
+          talker.speak(description);
+        } else {
+          let age = 0;
+          people.forEach(person => {
+            age += person.faceAttributes.age;
+          });
+          let averageAge = (age / res.length).toFixed(0);
 
-        let generalDescription = 'Ahora mismo tienes a ' + (res.length === 1 ? 'una persona' : res.length + ' personas') + ' delante.';
-
-        talker.speak(generalDescription, function () {
-          if (res.length < 4) { // avoid parsing too many people
-            let description = describeOxfordPeople(res);
-            talker.speak(description);
-          } else {
-            let age = 0;
-            people.forEach(person => {
-              age += person.faceAttributes.age;
-            });
-            let averageAge = (age / res.length).toFixed(0);
-
-            talker.speak('Su edad media es de unos ' + averageAge + ' anyos y son casi todos hombres');
-          }
-        });
-      } else {
-        const vision = require('node-cloud-vision-api');
-
-        const GOOGLE_VISION_API_TOKEN = process.env.GOOGLE_VISION_API_TOKEN;
-
-        if (!GOOGLE_VISION_API_TOKEN) {
-          console.error('You need to set a GOOGLE_VISION_API_TOKEN env var');
-          process.exit(1);
+          talker.speak('Su edad media es de unos ' + averageAge + ' anyos y son casi todos hombres');
         }
+      });
+    } else {
+      const req = new vision.Request({
+        image: new vision.Image(photoFileName),
+        features: [
+          new vision.Feature('LABEL_DETECTION', 10),
+          new vision.Feature('LANDMARK_DETECTION', 5),
+          new vision.Feature('LOGO_DETECTION', 5)
+        ]
+      });
 
-        vision.init({auth: GOOGLE_VISION_API_TOKEN});
+      vision.annotate(req).then((res) => {
+        console.log(JSON.stringify(res));
 
-        const req = new vision.Request({
-          image: new vision.Image(photoFileName),
-          features: [
-            new vision.Feature('LABEL_DETECTION', 10),
-            new vision.Feature('LANDMARK_DETECTION', 5),
-            new vision.Feature('LOGO_DETECTION', 5)
-          ]
-        });
+        if (Array.isArray(res.responses) && res.responses.length > 0) {
+          let response = res.responses[0];
 
-        vision.annotate(req).then((res) => {
-          console.log(JSON.stringify(res));
-
-          if (Array.isArray(res.responses) && res.responses.length > 0) {
-            let response = res.responses[0];
-
-            if (Array.isArray(response.logoAnnotations) && response.logoAnnotations.length > 0) {
-              let brand = response.logoAnnotations[0].description;
-              talker.speak('Tienes delante un producto de la marca ' + brand);
-            } else {
-              talker.speak('Lo siento. No he encontrado nada.');
-            }
+          if (Array.isArray(response.logoAnnotations) && response.logoAnnotations.length > 0) {
+            let brand = response.logoAnnotations[0].description;
+            talker.speak('Tienes delante un producto de la marca ' + brand);
           } else {
-            talker.speak('Lo siento. No he encontrado nada.');
+            talker.speak('Lo siento. No he encontrado nada interesante.');
           }
-        }, (e) => {
-          console.log('Error: ', e);
-        });
-      }
-    }, (e) => {
-      console.log('Error: ', e);
-    });
-  }
+        } else {
+          talker.speak('Lo siento. No he encontrado nada.');
+        }
+      }, (e) => {
+        console.log('Error: ', e);
+      });
+    }
+  }, (e) => {
+    console.log('Error: ', e);
+  });
 });
 
 function describeOxfordPeople(people) {
