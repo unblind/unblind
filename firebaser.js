ecefa0312aa89fc4f44adbf3ba06a9692a252401
Guido García <guido.garciabernardo@telefonica.com>
2016-02-26 12:11:30 +0100

    ignore first firebase message

--- firebaser.js
+++ firebaser.js
@@ -5,10 +5,14 @@ const firebase = require('firebase'),
 
 const myFirebaseRef = new Firebase('https://myeyes.firebaseio.com/');
 
+// XXX firebase notifies the current value after subscription. we ignore it here
+let firstMessage = true;
 myFirebaseRef.child('message').on('value', (snapshot) => {
   let text = snapshot.val();
-  if (text) {
+  if (!firstMessage && text) {
     console.log('New message: %s', text);
     talker.speak(text);
   }
+
+  firstMessage = false;
 });
